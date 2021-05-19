22095590132205fa424dfdc789ed9666eee9f39c
AVR216 <AVR216@avr216.github.com>
2021-05-18 22:46:49 -0500

    metodo signup del empleado

--- src/controllers/employeesController.js
+++ src/controllers/employeesController.js
@@ -1,6 +1,69 @@
+'use strict'
+
+const validator = require('validator');
+
+const Employee = require('../models/empleado');
+
 const controller = {
-    list: (req, res) =>{
+    list: (req, res) => {
         res.send('listado de empleados');
+    },
+    signup: (req, res) => {
+        const params = req.body;
+
+        //validando los datos recibidos
+        try {
+            var validate_name = !validator.isEmpty(params.name);
+            var validate_lastName = !validator.isEmpty(params.last_name);
+            var validate_phone = !validator.isEmpty(params.phone);
+            var validate_account = !validator.isEmpty(params.account);
+            var validate_cc = !validator.isEmpty(params.cc);
+            var validate_email = !validator.isEmpty(params.email);
+            var validate_password = !validator.isEmpty(params.password);
+
+        } catch (error) {
+            res.status(404).send({
+                status: 'error',
+                message: 'Datos incompletos, no se puede hacer el registro',
+                error
+            });
+        }
+        if (validate_name && validate_lastName && validate_phone && validate_account &&
+            validate_cc && validate_email && validate_password) {
+
+            const employee = new Employee();
+            employee.name = params.name;
+            employee.last_name = params.last_name;
+            employee.phone = params.phone;
+            employee.account = params.account;
+            employee.cc = params.cc;
+            employee.email = params.email;
+            employee.password = params.password;
+
+            employee.save((err, empStrored) => {
+                if (err || empStrored) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'El empleado no pudo ser registrado'
+                    });
+                }
+                return res.status(200).send({
+                    status: 'success',
+                    message: 'Empleado registrado exitosamente',
+                    empStrored
+                });
+            });
+            return res.status(200).send({
+                status: 'success',
+                message: 'Empleado registrado exitosamente',
+                employee
+            });
+        } else {
+            return res.status(404).send({
+                status: 'error',
+                message: 'Datos no validos'
+            });
+        }
     }
 };
 
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -6,4 +6,7 @@ const router = Router();
 //rutas get
 router.get('/list', controller.employees.list);
 
+//post
+router.post('/signup', controller.employees.signup);
+
 module.exports = router;
